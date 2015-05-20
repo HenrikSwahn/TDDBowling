85797fe6fff3ed7463938e543c20e950ebc3259e
HenrikSwahn <h_swahn@hotmail.com>
2015-05-20 13:27:42 +0200

    First test passed

--- src/Main/Frame.java
+++ src/Main/Frame.java
@@ -1,7 +1,26 @@
 package Main;
 
+import java.util.ArrayList;
+
 /**
  * Created by henrik on 20/05/15.
  */
 public class Frame {
+
+    private ArrayList<Integer> _throws;
+
+    public Frame(int throwOne, int throwTwo) {
+
+        _throws = new ArrayList<Integer>(2);
+        _throws.add(throwOne);
+        _throws.add(throwTwo);
+    }
+
+    public int getThrowOne() {
+        return _throws.get(0);
+    }
+
+    public int getThrowTwo() {
+        return _throws.get(1);
+    }
 }
--- src/Tests/FrameTest.java
+++ src/Tests/FrameTest.java
@@ -1,13 +1,27 @@
 package Tests;
 
+import Main.Frame;
+import org.junit.Before;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 public class FrameTest {
 
+    private Frame f;
+
+    @Before
+    public void setup() {
+        f = new Frame(4,5);
+    }
+
     @Test
-    public void test() {
-        assertEquals(1,1);
+    public void test_constructor() {
+
+        int throwOne = f.getThrowOne();
+        int throwTwo = f.getThrowTwo();
+
+        assertEquals(4, throwOne);
+        assertEquals(5, throwTwo);
+
     }
 }

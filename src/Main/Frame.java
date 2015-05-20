c69a1609fc0a908e231cf194f8dd0bf40a88eced
HenrikSwahn <h_swahn@hotmail.com>
2015-05-20 13:45:25 +0200

    First user story tested + one test of FrameScore

--- src/Main/Frame.java
+++ src/Main/Frame.java
@@ -1,5 +1,6 @@
 package Main;
 
+import org.w3c.dom.ranges.RangeException;
 import java.util.ArrayList;
 
 /**
@@ -12,6 +13,14 @@ public class Frame {
     public Frame(int throwOne, int throwTwo) {
 
         _throws = new ArrayList<Integer>(2);
+
+        if((throwOne + throwTwo) > 10) {
+            throw new RangeException((short) -1, "Sum cannot be larger than ten");
+        }
+
+        if((throwOne < 0) || throwTwo < 0) {
+            throw new RangeException((short) -1, "Value cannot be less than zero");
+        }
         _throws.add(throwOne);
         _throws.add(throwTwo);
     }
@@ -22,5 +31,9 @@ public class Frame {
 
     public int getThrowTwo() {
         return _throws.get(1);
+    }
+
+    public int getFrameScore() {
+        return _throws.get(0) + _throws.get(1);
     }
 }
--- src/Tests/FrameTest.java
+++ src/Tests/FrameTest.java
@@ -3,19 +3,16 @@ package Tests;
 import Main.Frame;
 import org.junit.Before;
 import org.junit.Test;
+import org.w3c.dom.ranges.RangeException;
 import static org.junit.Assert.*;
 
 public class FrameTest {
 
-    private Frame f;
-
-    @Before
-    public void setup() {
-        f = new Frame(4,5);
-    }
-
+    //Testing Frame user story
     @Test
-    public void test_constructor() {
+    public void test_constructor_validValuesGiven() {
+
+        Frame f = new Frame(4,5);
 
         int throwOne = f.getThrowOne();
         int throwTwo = f.getThrowTwo();
@@ -23,5 +20,36 @@ public class FrameTest {
         assertEquals(4, throwOne);
         assertEquals(5, throwTwo);
 
+    }
+
+    //Test Frame user story
+    @Test(expected = RangeException.class)
+    public void test_constructor_toLargeValuesGiven() {
+
+        Frame f = new Frame(8,7);
+    }
+
+    //Test Frame user story
+    @Test(expected = RangeException.class)
+    public void test_constructor_firstValueGivenNegative() {
+
+        Frame f = new Frame(-1, 5);
+    }
+
+    //Test Frame user story
+    @Test(expected = RangeException.class)
+    public void test_constructor_secondValueGivenNegativ() {
+
+        Frame f = new Frame(2, -1);
+    }
+
+    //Testing FrameScore user story
+    @Test
+    public void test_FrameScore_wantSumOfThrows() {
+
+        Frame f = new Frame(3,3);
+
+        int result = f.getFrameScore();
+        assertEquals(6, result);
     }
 }
